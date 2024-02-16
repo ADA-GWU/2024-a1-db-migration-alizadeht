2486e755b3f12ea9d4fc27dc9782864a8f860d89
alizadeht <turalalizadeh4@gmail.com>
2024-02-16 22:53:46 +0400

    Creation of Tables

---
+++ tablecreation.sql
@@ -0,0 +1,20 @@
+CREATE TABLE STUDENTS (
+  ST_ID SERIAL PRIMARY KEY,
+  ST_NAME VARCHAR(20),
+  ST_LAST VARCHAR(20)
+);
+
+CREATE TABLE INTERESTS (
+  STUDENT_ID INT,
+  INTEREST VARCHAR(20),
+  FOREIGN KEY (STUDENT_ID) REFERENCES STUDENTS(ST_ID)
+);
+INSERT INTO STUDENTS (ST_ID, ST_NAME, ST_LAST) VALUES
+(1, 'Konul', 'Gurbanova'),
+(2, 'Shahnur', 'Isgandarli'),
+(3, 'Natavan', 'Mammadova');
+
+INSERT INTO INTERESTS (STUDENT_ID, INTEREST) VALUES
+(1, 'Tennis'), (1, 'Literature'), (1, 'Chemistry'),
+(2, 'Math'), (2, 'Tennis'), (2, 'Football'),
+(3, 'Math'), (3, 'Music'), (3, 'Chess');
